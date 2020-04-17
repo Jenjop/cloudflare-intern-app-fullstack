0cdf93ab361f5f1f38d8a56fc88e8036ef682433
Jenjo <longinuspun@gmail.com>
2020-04-16 17:00:00 -0700

    Cookies
    Successfully saves cookies now, but to the variant

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+	// console.log("~~BEGIN~~")
 	const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
 
 	// let response = await fetch(url).then(res => {return res})
@@ -20,72 +21,85 @@ async function handleRequest(request) {
 	// const variant = variants[Math.floor(Math.random() * variants.length)]
 	// return fetch(variant)
 
-	let response = await fetch(url).then(res => res.json())
+	// let r = await fetch(request)
+	// const cooks = `rand=hello; Expires=Wed, 22 Apr 2020 00:0:00 GMT; Path='Derp'`
+	// r = new Response(r.body, r)
+	// r.headers.set('Set-Cookie', cooks)
+
+	// return r
+
+	let response = await fetch(url).then(res => res.json()) //Fetch from url
+	// let res = await fetch(url)
+	// let response = await res.json()
 	const variants = response['variants']
 
+
+
+	//Use saved variant from cookie if available
+
+	let cook = null
 	const cookie = getCookie(request, 'ind')
 	if (cookie){
-		console.log(cookie)
+		console.log('Found Cookie: ' + cookie)
 		ind = cookie
 	}
 	else {
 		ind = Math.floor(Math.random() * variants.length)
-		response = new Response(response.body, response)
-		response.headers.set('Set-Cookie', `ind=${ind}`)
-		console.log('\n\nNEWW\n\n')
-		console.log(request.headers.get('Cookie'))
-		console.log('New Cookie' + ind)
+		cook = `ind=${ind};Expires=Wed, 22 Apr 2020 00:00:00 GMT; Path='/'`
+		console.log('New Cookie: ' + cook)
 	}
 
 
-	// if (ind == -1){
-	// 	ind = Math.floor(Math.random() * variants.length)
-	// 	document.cookie = `${ind}`
-	// }
-	// else{
-	// 	console.log(document.cookie)
-	// 	let cookiearray = document.cookie.split(';')
-	// }
+	//ind = Math.floor(Math.random() * variants.length)
 
 
-
-	console.log('Random Index: ' + ind)
+	//Fetch from the picked variant
+	// console.log('Random Index: ' + ind)
 	let variant = variants[ind]
 	let variant_response = await fetch(variant)
-	return new HTMLRewriter()
+
+	//Replace/rewrite html fetched
+	let r = new HTMLRewriter()
 	.on('title', new PageTitleHandler())
 	.on('h1#title', new MainTitleHandler())
 	.on('p#description', new DescriptionHandler())
 	.on('a#url', new LinkHandler())
 	.transform(variant_response)
-	return fetch(variant)
+
+	if (cook){
+		r = new Response(r.body, r)
+		r.headers.set('Set-Cookie', cook)
+	}
+
+	return r
+	// return fetch(variant)
 }
 
 
 class PageTitleHandler {
 	element(element){
-		console.log(`Incoming Element: ${element.tagName}`)
+		// console.log(`Incoming Element: ${element.tagName}`)
 		element.setInnerContent('Custom Title!')
 	}
 }
 
 class MainTitleHandler {
 	element(element){
-		console.log(`Incoming Element: ${element.tagName}`)
+		// console.log(`Incoming Element: ${element.tagName}`)
 		element.setInnerContent(`Welcome To The ${ind? 'Green' : 'Blue'} Variant!`)
 	}
 }
 
 class DescriptionHandler {
 	element(element){
-		console.log(`Incoming Element: ${element.tagName}`)
+		// console.log(`Incoming Element: ${element.tagName}`)
 		element.setInnerContent('There is no description. This is not a plug.')
 	}
 }
 
 class LinkHandler {
 	element(element){
-		console.log(`Incoming Element: ${element.tagName}`)
+		// console.log(`Incoming Element: ${element.tagName}`)
 		element.setInnerContent('Go to my ~GitHub~')
 		element.setAttribute('href', 'https://github.com/jenjop')
 		element.setAttribute('target', '_blank')
@@ -93,9 +107,9 @@ class LinkHandler {
 }
 
 function getCookie(request, name) {
+	// console.log("Get Cookie-S")
 	let result = null
 	let cookieString = request.headers.get('Cookie')
-	console.log(cookieString)
 	if (cookieString){
 		let cookies = cookieString.split(';')
 		cookies.forEach(cookie => {
@@ -104,9 +118,26 @@ function getCookie(request, name) {
 				let cookieVal = cookie.split('=')[1]
 				result = cookieVal
 			}
+			// console.log('    ' + cookieName)
 		})
 	}
+	// console.log("Get Cookie-E")
 	return result
 }
+// function getCookie(request, name) {
+//   let result = null
+//   let cookieString = request.headers.get('Cookie')
+//   if (cookieString) {
+//     let cookies = cookieString.split(';')
+//     cookies.forEach(cookie => {
+//       let cookieName = cookie.split('=')[0].trim()
+//       if (cookieName === name) {
+//         let cookieVal = cookie.split('=')[1]
+//         result = cookieVal
+//       }
+//     })
+//   }
+//   return result
+// }
 
 // function saveCookie(request, name, value)
